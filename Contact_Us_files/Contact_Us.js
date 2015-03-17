65e0e0ac3ed4512a01a4d5eb3aab20a6ab2b98cd
Chris Hanson <coolchris1225@chriss-mbp.campus.tayloru.edu>
2015-03-16 22:57:28 -0400

    techligning v1

---
+++ Contact_Us_files/Contact_Us.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20100721
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_2:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_5:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_7:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_0:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_6:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),stroke_0:new IWEmptyStroke(),shadow_1:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_3:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_4:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Contact_Us_files/Contact_UsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Contact_Us_files','shapeimage_4','0');IMpreload('Contact_Us_files','shapeimage_5','0');IMpreload('Contact_Us_files','shapeimage_6','0');IMpreload('Contact_Us_files','shapeimage_7','0');IMpreload('Contact_Us_files','shapeimage_8','0');applyEffects()}
---
+++ Remote_Assistance_files/Remote_Assistance.js
@@ -0,0 +1,9 @@
+// Created by iWeb 2.0.4 local-build-20100721
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_4:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_0:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_2:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),stroke_0:new IWEmptyStroke(),shadow_5:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_7:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_6:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_3:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_1:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000}),shadow_8:new IWShadow({blurRadius:0,offset:new IWPoint(0.0000,-0.0000),color:'#000000',opacity:0.640000})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Remote_Assistance_files/Remote_AssistanceMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');IMpreload('Remote_Assistance_files','shapeimage_6','0');IMpreload('Remote_Assistance_files','shapeimage_7','0');IMpreload('Remote_Assistance_files','shapeimage_8','0');IMpreload('Remote_Assistance_files','shapeimage_9','0');applyEffects()}
